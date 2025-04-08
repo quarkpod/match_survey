b903b55edee7856ebdb457968d719d8e0f5d9cd9
quarkpod <quarks.uudddu@gmail.com>
2025-04-08 16:56:56 -0500

    starting setup

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+project = "match_survey"
+description = "Prepare and process polling responses on a soccer team."
+with open("config/requirements.txt") as requirement_file:
+    requirements = requirement_file.read().split()
+
+setup(
+    name=project,
+    description=description,
+    version="0.0.0",
+    author="",
+    author_email="",
+    install_requires=requirements,
+    packages=[project],
+    package_dir={
+        project: f'src/{project}'},
+    },
+)
