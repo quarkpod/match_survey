53660530b668e94836e04c61f57256cd70485dc9
quarkpod <quarks.uudddu@gmail.com>
2025-04-08 16:32:23 -0500

    basic configuration payload

---
+++ src/match_survey/payload/base_payload.py
@@ -0,0 +1,42 @@
+import sys
+import json
+import gspread
+import numpy as np
+import pandas as pd
+from match_survey.utils import load_json
+
+class Config:
+    template = {
+        'season_book_name': str,
+        'match_book_name': str,
+        'sheet_name': str,
+        'raw_data_file': str,
+        'index_col': list,
+        'player_cols': list,
+        'manager_regex': str,
+        'team_perf_col': str,
+        'opp_perf_col': str,
+        'ref_col': str,
+        'potm_col': str,
+        'manager_cols': list,
+        'survey_cols': list,
+        'season_book_url': str,
+        'creds_file': str,
+        'season_cols': list,
+    }
+    def __init__(self, config_filename: str):
+        self.config_filename = config_filename
+        self.config = self.load_json()
+        for k,v in self.config.items():  # attributes for items in config too
+            setattr(self, k, v)
+        for k,v in self.template.items():  # standard typing for important attr
+            setattr(self, k, v(self.config[k]))
+
+    def load_json(self) -> dict:
+        msg = f'{self.config_filename} not found'
+
+        return load_json(self.config_filename, msg, True)
+
+    def gather_attr(self, instance) -> dict:
+        for k,v in self.__dict__.items():
+            setattr(instance, k, v)
