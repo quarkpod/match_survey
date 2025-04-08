9bc3d692a12963d51f5571024c2c71cf9d54f94c
quarkpod <quarks.uudddu@gmail.com>
2025-04-08 16:33:23 -0500

    basic api caller

---
+++ src/match_survey/connect/base_api_caller.py
@@ -0,0 +1,33 @@
+import sys
+import json
+import gspread
+import numpy as np
+import pandas as pd
+from match_survey.payload.base_payload import Config
+
+class BaseAPICaller:
+    #book_strs = list()  # to be set in child classes, google sheet name or url
+    def __init__(self, config_filename: str):
+        Config(config_filename).gather_attr(self)
+        self.endpoint = str()
+        self.token = None
+        self.raw_data = list()
+        self.df = pd.DataFrame()
+        self.raw_data_file = str()
+
+    def __call__(self, save=False) -> pd.DataFrame:
+        self.get_token()
+        self.call_api()
+        self.save_raw_data()
+        #self.set_df(save)
+
+        #return self.df
+
+    def get_token(self) -> None:
+        raise NotYetImplemented
+
+    def call_api(self) -> None:
+        raise NotYetImplemented
+
+    def save_raw_data(self):
+        self.df.to_csv(self.raw_data_file)
